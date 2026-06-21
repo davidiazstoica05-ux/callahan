1976691f4be72d80e86153e74d58afe49cfa05c2
DavidFedora <davidiazstoica05@gmail.com>
2026-06-21 17:36:00 +0200

    feat: Creacion de los repositorios

---
+++ callahan-codigo-fuente/src/main/java/com/callahan/callahancodigofuente/repository/InteraccionRepository.java
@@ -0,0 +1,7 @@
+package com.callahan.callahancodigofuente.repository;
+
+import com.callahan.callahancodigofuente.models.Interaccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InteraccionRepository extends JpaRepository<Interaccion, Long> {
+}
---
+++ callahan-codigo-fuente/src/main/java/com/callahan/callahancodigofuente/repository/PreferenciasRepository.java
@@ -0,0 +1,7 @@
+package com.callahan.callahancodigofuente.repository;
+
+import com.callahan.callahancodigofuente.models.Preferencias;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PreferenciasRepository extends JpaRepository<Preferencias, Long> {
+}
